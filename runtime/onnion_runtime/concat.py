d29428cb858f46e1537631e8d361f455c8fb779b
ishiy <y.ishihara.1993@gmail.com>
2021-07-13 11:26:32 +0900

    Add tests for Concat

--- runtime/onnion_runtime/concat.py
+++ runtime/onnion_runtime/concat.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class Concat:
     def __init__(self, opset_version, **kwargs):
         self.version = opset_version
-        self.axis = kwargs['axis']
+        self.axis = kwargs["axis"]
 
     def run(self, *inputs):
         return [np.concatenate(inputs, axis=self.axis)]
