a258bde87d38d9f05b224eb60fc1802d03452fcd
ishiy <y.ishihara.1993@gmail.com>
2021-07-13 11:20:18 +0900

    Fix check

--- runtime/tests/test_exp.py
+++ runtime/tests/test_exp.py
@@ -8,7 +8,7 @@ def exp_checker(opset_version):
     v0 = np.random.randn(1, 2, 3, 4).astype(np.float32)
     [v1] = Exp(opset_version).run(v0)
 
-    check("Exp", dict(), {"v0": v0}, {"v1": v1}, opset_version)
+    check("Exp", dict(), [v0], [v1], opset_version)
 
 
 def test_exp_00():
--- runtime/tests/utils.py
+++ runtime/tests/utils.py
@@ -1,4 +1,5 @@
 import tempfile
+from typing import Any, Dict, List
 
 import numpy as np
 import onnx
@@ -10,23 +11,38 @@ def convert_type(dtype):
     return mapping.NP_TYPE_TO_TENSOR_TYPE[dtype]
 
 
-def run_onnx(model, inputs, outputs):
+def run_onnx(model, inputs, output_names):
     checker.check_model(model)
-    output_names = [k for k in outputs]
     with tempfile.NamedTemporaryFile(mode="w") as f:
         onnx.save(model, f.name)
         sess = onnxruntime.InferenceSession(f.name)
         return sess.run(output_names, inputs)
 
 
-def check(op_name, attrs, inputs, outputs, opset_version, max_error=1e-4):
-    node = helper.make_node(op_name, inputs.keys(), outputs.keys(), attrs)
-    input_tensors = [helper.make_tensor_value_info(k, convert_type(inputs[k].dtype), list(inputs[k].shape)) for k in inputs]
-    output_tensors = [helper.make_tensor_value_info(k, convert_type(outputs[k].dtype), list(outputs[k].shape)) for k in outputs]
+def check(
+    op_name: str,
+    attrs: Dict[str, Any],
+    input_values: List[np.array],
+    output_values: List[np.array],
+    opset_version: int,
+    max_error=1e-4,
+):
+    input_names = [f"input{i}" for i, _ in enumerate(input_values)]
+    output_names = [f"output{i}" for i, _ in enumerate(output_values)]
+    node = helper.make_node(op_name, input_names, output_names, **attrs)
+    input_tensors = [
+        helper.make_tensor_value_info(n, convert_type(v.dtype), list(v.shape)) for n, v in zip(input_names, input_values)
+    ]
+    output_tensors = [
+        helper.make_tensor_value_info(n, convert_type(v.dtype), list(v.shape)) for n, v in zip(output_names, output_values)
+    ]
     graph = helper.make_graph([node], "test_graph", input_tensors, output_tensors)
     opset_imports = [helper.make_opsetid("", opset_version)]
     model = helper.make_model(graph, opset_imports=opset_imports)
 
-    results = run_onnx(model, inputs, outputs)
-    for a, b in zip(results, [outputs[k] for k in outputs]):
+    inputs = dict()
+    for n, v in zip(input_names, input_values):
+        inputs[n] = v
+    results = run_onnx(model, inputs, output_names)
+    for a, b in zip(results, output_values):
         assert np.all(abs(a - b) < max_error)
