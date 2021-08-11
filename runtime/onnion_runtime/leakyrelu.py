2113aa196a7082f02a33c0f232867f3ebec161b6
ishiy <y.ishihara.1993@gmail.com>
2021-08-11 14:30:18 +0900

    Support LeakyRelu

---
+++ runtime/onnion_runtime/leakyrelu.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class LeakyRelu:
+    def __init__(self, opset_version, **kwargs):
+        self.version = opset_version
+        self.alpha = kwargs.get("alpha", 0.01)
+
+    def run(self, x):
+        y = np.clip(x, 0, np.inf) + np.clip(x, -np.inf, 0) * self.alpha
+        return [y]
