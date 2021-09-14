371d0f365fa5ca7678d892de5290459057eaf16b
ishiy <y.ishihara.1993@gmail.com>
2021-09-14 13:26:12 +0900

    Fix ScatterND

--- runtime/onnion_runtime/scatternd.py
+++ runtime/onnion_runtime/scatternd.py
@@ -14,6 +14,7 @@ class ScatterND:
 # https://github.com/onnx/onnx/blob/07c494bf077e9e4a7898119f28a50585469ad4cd/onnx/backend/test/case/node/scatternd.py#L15-L31
 # Copyrights (c) ONNX Project Contributers
 # License: Apache-2.0
+# NOTE: Modify i -> tuple(i), indices[i] -> tuple(indices[i])
 def scatter_nd_impl(data, indices, updates, reduction="none"):  # type: ignore
 
     # Check tensor shapes
@@ -25,9 +26,9 @@ def scatter_nd_impl(data, indices, updates, reduction="none"):  # type: ignore
     for i in np.ndindex(indices.shape[:-1]):
         # NOTE: The order of iteration in this loop is not specified.
         if reduction == "add":
-            output[indices[i]] += updates[i]
+            output[tuple(indices[i])] += updates[tuple(i)]
         elif reduction == "mul":
-            output[indices[i]] *= updates[i]
+            output[tuple(indices[i])] *= updates[tuple(i)]
         else:
-            output[indices[i]] = updates[i]
+            output[tuple(indices[i])] = updates[tuple(i)]
     return output
