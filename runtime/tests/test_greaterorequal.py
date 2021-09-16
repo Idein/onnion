35a5d18ec621d92fbadc74627bde2d42faa81282
ishiy <y.ishihara.1993@gmail.com>
2021-09-16 10:28:28 +0900

    Format

--- runtime/tests/test_greaterorequal.py
+++ runtime/tests/test_greaterorequal.py
@@ -9,17 +9,18 @@ def test_greaterorequal_00():
 
     x = np.random.randn(3, 4, 5).astype(np.float32)
     y = np.random.randn(3, 4, 5).astype(np.float32)
-    inputs = [x,y]
-    outputs = GreaterOrEqual(opset_version).run(x,y)
+    inputs = [x, y]
+    outputs = GreaterOrEqual(opset_version).run(x, y)
 
     check("GreaterOrEqual", dict(), inputs, outputs, opset_version)
+
 
 def test_greaterorequal_01():
     opset_version = 13
 
     x = np.random.randn(3, 4, 5).astype(np.float32)
     y = np.random.randn(5).astype(np.float32)
-    inputs = [x,y]
-    outputs = GreaterOrEqual(opset_version).run(x,y)
+    inputs = [x, y]
+    outputs = GreaterOrEqual(opset_version).run(x, y)
 
     check("GreaterOrEqual", dict(), inputs, outputs, opset_version)
