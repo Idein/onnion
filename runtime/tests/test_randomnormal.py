2e906c4819133d7154dfb5a0071a7fc83b04ece3
ishiy <y.ishihara.1993@gmail.com>
2022-01-26 13:40:27 +0900

    Fix tests

--- runtime/tests/test_randomnormal.py
+++ runtime/tests/test_randomnormal.py
@@ -1,19 +1,21 @@
 import pytest
 from onnion_runtime import RandomNormal
 
-from .utils import check
+from .utils import check, on_arm32
 
 
+@pytest.mark.skipif(on_arm32(), reason="need to pass tests on x86_64")
 @pytest.mark.xfail(raises=AssertionError)
 def test_randomnormal_00():
     opset_version = 13
-    attrs = {"dtype": 1, "mean": 0.0, "scale": 1.0, "seed": 0.0, "shape": [2, 3]}
+    attrs = {"dtype": 1, "mean": 5.0, "scale": 3.0, "seed": 0.0, "shape": [2, 3]}
 
     inputs = []
 
     check(RandomNormal, opset_version, attrs, inputs)
 
 
+@pytest.mark.skipif(on_arm32(), reason="need to pass tests on x86_64")
 @pytest.mark.xfail(raises=AssertionError)
 def test_randomnormal_01():
     opset_version = 13
