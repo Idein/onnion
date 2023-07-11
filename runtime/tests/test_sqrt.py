435e8921c038fa12bf0ea68cc4d57a5139584c3c
ishiy <y.ishihara.1993@gmail.com>
2023-07-11 16:06:21 +0900

    Support Sqrt

---
+++ runtime/tests/test_sqrt.py
@@ -0,0 +1,23 @@
+import numpy as np
+from onnion_runtime import Sqrt
+
+from .utils import check
+
+
+def test_relu_00():
+    opset_version = 14
+    attrs = dict()
+
+    x = np.abs(np.random.randn(3, 4, 5).astype(np.float32))
+    inputs = [x]
+
+    check(Sqrt, opset_version, attrs, inputs)
+
+def test_relu_01():
+    opset_version = 14
+    attrs = dict()
+
+    x = np.array([1, 4, 9]).astype(np.float32)
+    inputs = [x]
+
+    check(Sqrt, opset_version, attrs, inputs)
