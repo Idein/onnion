76f4826e004ad642bec2187e689b7c91c90e1b9e
takayuki goto <takayuki@idein.com>
2023-07-18 16:09:23 +0900

    Improve: copy BatchNormalization from onnx test
    Add: test code is ported from the onnx spec

--- runtime/onnion_runtime/batchnorm.py
+++ runtime/onnion_runtime/batchnorm.py
@@ -1,17 +1,29 @@
 import numpy as np
 
-class BatchNorm:
+class BatchNormalization:
     def __init__(self, opset_version, **kwargs):
         self.version = opset_version
-        self.gamma = kwargs.get("gamma")
-        self.beta = kwargs.get("beta")
-        self.eps = kwargs.get("eps")
+        assert self.version >= 15, f"Now, only opset_versions (>= 15) are supported"
+        self.epsilon = kwargs.get("epsilon", 1e-5)
+        assert 0 == kwargs.get("training_mode", 0), 'Non-zero values for training_mode are not supported'
 
-    def run(self, xs):
-        mean = np.average(xs)
-        var  = np.var(xs)
-        # normalize
-        xhat = (xs - mean) / np.sqrt(var + self.eps)
-        # scale and shift
-        return [self.gamma * xhat + self.beta]
+    def run(self, x, scale, bias, input_mean, input_var):
+        """
+        Perform batch normalization
+
+        This implementation is copied from onnion test code:
+        https://github.com/onnx/onnx/blob/13f02aacb5d10495eed55a59729d9d2993db43bf/onnx/backend/test/case/node/batchnorm.py#L12
+        Arguments are same to the original implementation,
+        but output is a singleton list wraps the output of the original.
+
+        Returns:
+          List of an element which is the ouput tensor of the same shape as x
+        """
+        dims_x = len(x.shape)
+        dim_ones = (1,) * (dims_x - 2)
+        scale = scale.reshape(-1, *dim_ones)
+        bias = bias.reshape(-1, *dim_ones)
+        mean = input_mean.reshape(-1, *dim_ones)
+        var = input_var.reshape(-1, *dim_ones)
+        return [scale * (x - mean) / np.sqrt(var + self.epsilon) + bias]
 
---
+++ runtime/tests/test_batchnorm.py
@@ -0,0 +1,20 @@
+import numpy as np
+from onnion_runtime import BatchNormalization
+
+from .utils import check
+
+def test_batchnorm_00():
+    opset_version = 15
+    attrs = dict()
+
+    # input size: (2, 3, 4, 5)
+    x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+    s = np.random.randn(3).astype(np.float32)
+    bias = np.random.randn(3).astype(np.float32)
+    mean = np.random.randn(3).astype(np.float32)
+    var = np.random.rand(3).astype(np.float32)
+
+    inputs = [x, s, bias, mean, var]
+    check(BatchNormalization, opset_version, attrs, inputs)
+
+
