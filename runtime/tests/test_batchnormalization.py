6c9f7ba7e882082c4bacb27f1a8573f1a17eb328
takayuki goto <takayuki@idein.com>
2023-07-19 20:45:41 +0900

    Add: test cases for opset version 14 and 15

--- runtime/tests/test_batchnormalization.py
+++ runtime/tests/test_batchnormalization.py
@@ -34,3 +34,33 @@ def test_batchnormalization_01():
     inputs = [x, s, bias, mean, var]
     attrs["epsilon"] = epsilon
     check(BatchNormalization, opset_version, attrs, inputs)
+
+
+def test_batchnormalization_02():
+    opset_version = 14
+    attrs = dict()
+
+    # input size: (2, 3, 4, 5)
+    x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+    s = np.random.randn(3).astype(np.float32)
+    bias = np.random.randn(3).astype(np.float32)
+    mean = np.random.randn(3).astype(np.float32)
+    var = np.random.rand(3).astype(np.float32)
+
+    inputs = [x, s, bias, mean, var]
+    check(BatchNormalization, opset_version, attrs, inputs)
+
+
+def test_batchnormalization_03():
+    opset_version = 15
+    attrs = dict()
+
+    # input size: (2, 3, 4, 5)
+    x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+    s = np.random.randn(3).astype(np.float32)
+    bias = np.random.randn(3).astype(np.float32)
+    mean = np.random.randn(3).astype(np.float32)
+    var = np.random.rand(3).astype(np.float32)
+
+    inputs = [x, s, bias, mean, var]
+    check(BatchNormalization, opset_version, attrs, inputs)
