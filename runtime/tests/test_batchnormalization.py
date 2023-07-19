c8fdee7fdc8673c0d6d4fa43755dfb37df12c723
takayuki goto <takayuki@idein.com>
2023-07-19 11:02:12 +0900

    Add: test case for BN

--- runtime/tests/test_batchnormalization.py
+++ runtime/tests/test_batchnormalization.py
@@ -17,3 +17,20 @@ def test_batchnormalization_00():
 
     inputs = [x, s, bias, mean, var]
     check(BatchNormalization, opset_version, attrs, inputs)
+
+
+def test_batchnormalization_01():
+    opset_version = 9
+    attrs = dict()
+
+    # input size: (2, 3, 4, 5)
+    x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+    s = np.random.randn(3).astype(np.float32)
+    bias = np.random.randn(3).astype(np.float32)
+    mean = np.random.randn(3).astype(np.float32)
+    var = np.random.rand(3).astype(np.float32)
+    epsilon = 1e-2
+
+    inputs = [x, s, bias, mean, var]
+    attrs["epsilon"] = epsilon
+    check(BatchNormalization, opset_version, attrs, inputs)
