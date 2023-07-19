fef9546708812eff5894cb139a9da7c871d7a706
takayuki goto <takayuki@idein.com>
2023-07-19 09:59:11 +0900

    Move: BN impl to free function

--- runtime/onnion_runtime/batchnorm.py
+++ runtime/onnion_runtime/batchnorm.py
@@ -13,19 +13,24 @@ class BatchNormalization:
         """
         Perform batch normalization
 
-        This implementation is copied from onnion test code:
-        https://github.com/onnx/onnx/blob/13f02aacb5d10495eed55a59729d9d2993db43bf/onnx/backend/test/case/node/batchnorm.py#L12
         Arguments are same to the original implementation,
         but output is a singleton list wraps the output of the original.
 
         Returns:
           List of an element which is the ouput tensor of the same shape as x
         """
-        dims_x = len(x.shape)
-        dim_ones = (1,) * (dims_x - 2)
-        scale = scale.reshape(-1, *dim_ones)
-        bias = bias.reshape(-1, *dim_ones)
-        mean = input_mean.reshape(-1, *dim_ones)
-        var = input_var.reshape(-1, *dim_ones)
-        return [scale * (x - mean) / np.sqrt(var + self.epsilon) + bias]
+        return [_batchnormalization(x, scale, bias, input_mean, input_var, self.epsilon)]
+
+# The following code has been copied from
+# https://github.com/onnx/onnx/blob/13f02aacb5d10495eed55a59729d9d2993db43bf/onnx/backend/test/case/node/batchnorm.py#L12
+# Copyrights (c) ONNX Project Contributers
+# License: Apache-2.0
+def _batchnormalization(x, s, bias, mean, var, epsilon):  # type: ignore
+    dims_x = len(x.shape)
+    dim_ones = (1,) * (dims_x - 2)
+    s = s.reshape(-1, *dim_ones)
+    bias = bias.reshape(-1, *dim_ones)
+    mean = mean.reshape(-1, *dim_ones)
+    var = var.reshape(-1, *dim_ones)
+    return s * (x - mean) / np.sqrt(var + epsilon) + bias
 
